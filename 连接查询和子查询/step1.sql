152f949832159463649a1dbdc3f7bb3c0c7519ac
lovekang3344 <1492303864@qq.com>
2023-06-21 20:14:13 +0800

    修复错误

--- 连接查询和子查询/step1.sql
+++ 连接查询和子查询/step1.sql
@@ -1,6 +1,13 @@
 USE School;
+
 ########## 查询数据表中学生姓名和对应的班级 ##########
 #请在此处添加实现代码
 ########## Begin ##########
+
 select tb_student.name as studentName, tb_class.name as className
+from tb_student join tb_class on tb_student.class_id = tb_class.id;
+
+
+
 ########## End ##########
+
