7e013baf7db2377550a70455b8a4042b9abc46a2
lovekang3344 <1492303864@qq.com>
2023-08-21 07:02:37 +0000

    update 单表查询（一）/step3.sql. 来自胡lt同志的提醒，已经改过错误，欢迎大家提问
    Signed-off-by: lovekang3344 <1492303864@qq.com>

--- 单表查询（一）/step3.sql
+++ 单表查询（一）/step3.sql
@@ -2,7 +2,5 @@ USE Company;
 #请在此处添加实现代码
 ########## Begin ##########
 ########## retrieving the Name and Salary ##########
-select Name,Salary from tb_emp;
-########## retrieving all the table ##########
-select * from tb_emp;
+select Name,Salary from tb_emp where Salary between 3000 and 5000;
 ########## End ##########
